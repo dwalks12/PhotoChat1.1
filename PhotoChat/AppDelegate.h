ae392c758ef3600f28c8d5c1e6996f3e751563c9
Dawson Walker <dawsonwalker@w134-87-178-167.wireless.uvic.ca>
2014-06-02 12:19:34 -0700

    Initial Commit

---
+++ PhotoChat/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PhotoChat
+//
+//  Created by Dawson Walker on 2014-06-02.
+//  Copyright (c) 2014 Dawson Walker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
